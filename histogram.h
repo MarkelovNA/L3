0acb014702c88322bd1e262b03b2e3a7ffccaf51
MarkelovNA <marknik139@gmail.com>
2020-05-24 14:43:11 +0300

    code: change input_numbers

--- histogram.h
+++ histogram.h
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 
-vector<double> input_numbers(size_t count);
+vector<double> input_numbers(istream& in, size_t count);
 void find_minmax(vector<double> numbers, double& min, double& max);
 vector<size_t> make_histogram(vector<double>numbers, size_t bin_count);
 void show_histogram_text(vector<size_t> bins);
