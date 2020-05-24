89f4772fb463b3fa13b1cd53a75e62963fc959aa
MarkelovNA <marknik139@gmail.com>
2020-05-24 17:15:39 +0300

    code: cURL linked to project

--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
 
 using namespace std;
 
 int main()
 {
+    curl_global_init(CURL_GLOBAL_ALL);
+
     const auto input = read_input(cin, true);
-
     const auto bins=make_histogram(input);
-
     double bin_height; //For module tests
     show_histogram_svg(bins, bin_height);
 
