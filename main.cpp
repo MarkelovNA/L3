99775c1652084639f999c699d87e3a4bbf9c66f1
MarkelovNA <marknik139@gmail.com>
2020-05-24 15:31:18 +0300

    code: change input (struct)

--- main.cpp
+++ main.cpp
@@ -5,20 +5,34 @@
 
 using namespace std;
 
-int main()
+struct Input
 {
-    size_t number_count;
+    vector <double> numbers;
+    size_t bin_count;
+};
+
+Input read_input(istream& in)
+{
+    Input data;
+
     cerr << "Enter number count: ";
-    cin >> number_count;
+    size_t number_count;
+    in >> number_count;
 
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(cin, number_count);
+    data.numbers = input_numbers(in, number_count);
 
-    size_t bin_count;
     cerr << "Enter column count: ";
-    cin >> bin_count;
+    in >> data.bin_count;
 
-    const auto bins=make_histogram(numbers, bin_count);
+    return data;
+}
+
+int main()
+{
+    const auto input = read_input(cin);
+
+    const auto bins=make_histogram(input.numbers,input.bin_count);
 
     double bin_height;
     show_histogram_svg(bins, bin_height);
