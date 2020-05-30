06ce4bef1a541e99d10ddb31d46966be998d275b
MarkelovNA <marknik139@gmail.com>
2020-05-30 13:36:16 +0300

    code: add major & minor versions

--- main.cpp
+++ main.cpp
@@ -10,9 +10,15 @@ using namespace std;
 int main()
 {
     DWORD info = GetVersion();
-    DWORD mask = 0b00000000'00000000'11111111'11111111;
+    DWORD mask = 0x0000ffff;
     DWORD version = info & mask;
+    DWORD platform = info >> 16;
+    DWORD mask_major = 0x0000ff;
+    DWORD version_major = version & mask_major;
+    DWORD version_minor = version >> 8;
     printf("Windows version is %u.\n", version);
+    printf("Version Major is %u.\n", version_major);
+    printf("Version Minor is %u.\n", version_minor);
     return 0;
 
     size_t number_count;
